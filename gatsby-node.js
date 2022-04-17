6da9f140c9c4fce2b407c600c2352daf1c53c762
J. David Mendoza <jdmendoza@swau.edu>
2022-04-17 18:13:30 -0500

    lib upgrade

--- gatsby-node.js
+++ gatsby-node.js
@@ -138,3 +138,21 @@ exports.createPages = async ({ graphql, actions }) => {
         throw err
     }
 }
+
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+    if (stage === 'build-html') {
+        actions.setWebpackConfig({
+            externals: getConfig().externals.concat(function (
+                { request },
+                callback
+            ) {
+                const regex = /^@?firebase(\/(.+))?/
+                // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
+                if (regex.test(request)) {
+                    return callback(null, 'commonjs ' + request) // <- use commonjs!
+                }
+                callback()
+            })
+        })
+    }
+}
